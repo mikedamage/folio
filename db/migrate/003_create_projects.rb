d1dea84c4d86c4774b6d70b0f4255bb53cb3e0e8
Mike Green <mike@fifthroomcreative.com>
2008-02-16 17:22:14 -0500

    created Project model

---
+++ db/migrate/003_create_projects.rb
@@ -0,0 +1,17 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.string :name
+      t.integer :category_id
+      t.integer :client_id
+      t.string :url
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
