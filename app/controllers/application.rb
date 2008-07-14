4de4154bdb9faf88f671703c36c7fdc43f2467cb
Mike Green <mike.is.green@gmail.com>
2008-07-14 02:04:15 -0400

    generated authenticated user and session

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,8 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+	include AuthenticatedSystem
+	
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'c3b83a34c801b34993771f74291f1e95'
