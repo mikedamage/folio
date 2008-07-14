07f6a2b46b70d6dd939bb214809fae39f393f523
Mike Green <mike.is.green@gmail.com>
2008-07-14 01:48:53 -0400

    fixed data type problem in the in_rotation database migration

--- db/migrate/005_add_rotation_field_to_projects.rb
+++ db/migrate/005_add_rotation_field_to_projects.rb
@@ -1,6 +1,6 @@
 class AddRotationFieldToProjects < ActiveRecord::Migration
   def self.up
-		add_column :projects, :in_rotation, :boolean, :default => 1
+		add_column :projects, :in_rotation, :boolean, :default => true
   end
 
   def self.down
