b28a8ea71f8e7c45fca1ba285604c3791308453d
Mike Green <mike.is.green@gmail.com>
2008-10-03 14:50:23 -0400

    generated Projects controller with empty actions and views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,26 @@
+class ProjectsController < ApplicationController
+  def index
+  end
+
+  def images
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
