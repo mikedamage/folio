de6cd7b070122b48a8c96e189d5caf736fe0d211
Mike Green <mike.is.green@gmail.com>
2008-10-07 01:36:50 -0400

    installed jrails plugin as submodule, using jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  $('a.add_field').click(function() {
+    // Add another item to the Custom fields array on the New/Edit Category pages
+  });
+});
